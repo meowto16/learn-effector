ab92325b453f81352ebc9e37e6a10e200b6b754c
Maxim Kirshin <meowto16@gmail.com>
2022-07-03 11:34:29 +0500

    Run example from effector.dev

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('Hello from webpack')
+import {createEvent, createStore, createEffect, combine, sample} from 'effector'
+
+const nextPost = createEvent()
+
+const getCommentsFx = createEffect(async postId => {
+    const url = `posts/${postId}/comments`
+    const base = 'https://jsonplaceholder.typicode.com'
+    const req = await fetch(`${base}/${url}`)
+    return req.json()
+})
+
+const $postComments = createStore([])
+    .on(getCommentsFx.doneData, (_, comments) => comments)
+
+const $currentPost = createStore(1)
+    .on(getCommentsFx.done, (_, {params: postId}) => postId)
+
+const $status = combine(
+    $currentPost, $postComments, getCommentsFx.pending,
+    (postId, comments, isLoading) => isLoading
+        ? 'Loading post...'
+        : `Post ${postId} has ${comments.length} comments`
+)
+
+sample({
+    source: $currentPost,
+    clock: nextPost,
+    fn: postId => postId + 1,
+    target: getCommentsFx,
+})
+
+$status.watch(status => {
+    console.log(status)
+})
+// => Post 1 has 0 comments
+
+nextPost()
+
+// => Loading post...
+// => Post 2 has 5 comments
